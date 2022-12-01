b00d1df5aac9a83ccab8e82138bfd931f2441612
William <william-dechamps@hotmail.com>
2022-12-01 17:28:29 +0100

    update: debut SSR && update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function User(props) {
+  const { user } = props;
+  return <p>{user}</p>;
+}
 
 function App() {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    getGroups();
+  }, []);
+
+
+  const getGroups = () => {
+    fetch("http://localhost:8501/groupes", {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then(setGroups)
+      .catch((err) => alert("a problem has occured"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {groups.length > 0 &&
+          groups.map((group, i) => {
+            return (
+              <div key={'groupe_' + i}>
+                <h1> {"Groupe " + ++i}</h1>
+                {group.map((user, key) => (
+                  <User key={'person_' + i + '_' + key} user={user} />
+                ))}
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
